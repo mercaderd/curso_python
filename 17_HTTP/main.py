b515d8cc585d79e6138042fcae8a87b20fa3763d
Daniel <mercaderd@users.noreply.github.com>
2023-10-21 21:56:57 +0000

    API REST

--- 17_HTTP/main.py
+++ 17_HTTP/main.py
@@ -1,6 +1,7 @@
 # HTTP Requests
 
 import requests
+import json
 
 # Hacer una petición GET
 
@@ -19,7 +20,8 @@ print(r.status_code) # 404 Error porque el endpoint no existe
 
 r = requests.get('https://fakestoreapi.com/products')
 if r.status_code == requests.codes.ok:
-    print(r.json())
+    print(type(r.json()))
+    print(json.dumps(r.json()[0:2],indent=4))
 
 
 
