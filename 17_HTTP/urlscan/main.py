6e5f1e282ea3db7c25e3014a91fea439bb57b4b3
Daniel <mercaderd@users.noreply.github.com>
2023-10-22 18:55:43 +0000

    urlscan/main.py changes

--- 17_HTTP/urlscan/main.py
+++ 17_HTTP/urlscan/main.py
@@ -7,3 +7,5 @@ apikey = '' # API Key no es necesaria para pedir resultados
 
 if __name__ == '__main__':
     print(urlscan.result(apikey=apikey, uuid='8a8b07b8-d467-4bce-a28d-7764c7950792'))
+    urlscan.report(apikey=apikey, uuid='8a8b07b8-d467-4bce-a28d-7764c7950792')
+    
