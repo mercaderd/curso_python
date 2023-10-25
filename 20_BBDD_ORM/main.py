9970ef0f6da090b85f17bf1a118eea245233b7e1
Daniel <mercaderd@users.noreply.github.com>
2023-10-25 20:14:47 +0000

    bbdd

--- 20_BBDD_ORM/main.py
+++ 20_BBDD_ORM/main.py
@@ -4,12 +4,17 @@ from models import Persona
 def run():
     """Documentar"""
     # TODO: Documentar la función
-    nuevo_cliente = Persona(nombre='Victor',apellido='Fernandez',edad=33)
-    print(nuevo_cliente.pk)
-    db.session.add(nuevo_cliente)
-    print(nuevo_cliente.pk)
-    db.session.commit()
-    print(nuevo_cliente.pk)
+    with open('./datos.csv', encoding='utf-8') as f:
+        lineas = f.readlines()
+        for linea in lineas[1:]:
+            nombre,apellido,edad = linea.split(sep=',')
+            nuevo_cliente = Persona(nombre=nombre, apellido=apellido,edad=int(edad))
+            print(nuevo_cliente.pk)
+            db.session.add(nuevo_cliente)
+            print(nuevo_cliente.pk)
+            db.session.commit()
+            print(nuevo_cliente.pk)
+            print(nuevo_cliente)
 
 
 if __name__ == '__main__':
